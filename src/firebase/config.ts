b2f27be0baafde6ecbcbf6e8f4322e2a74058755
Robert Stewart <robstewart@my365.bellevue.edu>
2025-11-22 07:14:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7794071366-c1503",
+  "appId": "1:246427962952:web:eacc84bf6de33ebb5c927d",
+  "apiKey": "AIzaSyDooYVK6EOltAzJGeAYAX7fOOj9CLM5JaA",
+  "authDomain": "studio-7794071366-c1503.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "246427962952"
+};
