3ba27425c224255da4f86e5c996d8f9fa151b478
Robert Stewart <robstewart@my365.bellevue.edu>
2025-11-25 20:03:56 +0000

    fix the current error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-inter)', 'sans-serif'],
+        headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },
       colors: {
